40ca65be3dfed0d09462f98a0c2b337eab085af6
vaalss <valeria.hernandezmaldo@gmail.com>
2026-05-19 21:08:35 -0600

    visualización de matriz de adyacencia

--- Main.java
+++ Main.java
@@ -20,8 +20,9 @@ public class Main {
             System.out.println("1. Ruta más corta");
             System.out.println("2. Centro del grafo");
             System.out.println("3. Modificar conexiones");
-            System.out.println("4. Mostrar matriz");
-            System.err.println("5. Salir");
+            System.out.println("4. Mostrar matriz de adyacencia");
+            System.out.println("5. Mostrar matriz de Floyd");
+            System.err.println("6. Salir");
 
             System.out.print("Selecciona la opciones que deseas realizar: ");
             int option = scanner.nextInt();
@@ -29,15 +30,15 @@ public class Main {
 
             switch(option) {
                 case 1:
-                    System.out.print("Ciudad origen: ");
-                    String from = scanner.nextLine();
+                    System.out.print("\nCiudad origen: ");
+                    String from = scanner.nextLine().trim();
                     System.out.print("Ciudad destino: ");
                     String to = scanner.nextLine();
 
                     graph.shortestPath(from, to);
                     break;
                 case 2:
-                    System.out.println("Centro del grafo: " + graph.graphCenter());
+                    System.out.println("\nCentro del grafo: " + graph.graphCenter());
                     break;
                 case 3:
                     System.out.println("\n\t1. Eliminar conexión");
@@ -48,21 +49,22 @@ public class Main {
                     
                     if (modify_option == 1) {
                         System.out.print("\n\tCiudad origen: ");
-                        String from_ = scanner.nextLine();
+                        String from_ = scanner.nextLine().trim();
                         System.out.print("\tCiudad destino: ");
-                        String to_ = scanner.nextLine();
+                        String to_ = scanner.nextLine().trim();
 
                         graph.removeEdge(from_, to_);
                         graph.floyd();
                         System.out.println("\tConexión eliminada.");
                     } else if (modify_option == 2) {
                         System.out.print("\n\tCiudad origen: ");
-                        String _from = scanner.nextLine();
+                        String _from = scanner.nextLine().trim();
                         System.out.print("\tCiudad destino: ");
-                        String _to = scanner.nextLine();
+                        String _to = scanner.nextLine().trim();
 
                         System.out.print("\tDistancia: ");
                         int distance = scanner.nextInt();
+                        scanner.nextLine();
 
                         graph.addEdge(_from, _to, distance);
                         graph.floyd();
@@ -70,9 +72,14 @@ public class Main {
                     }
                     break;
                 case 4:
-                    graph.distanceMatrix();
+                    System.out.println("\nMatriz de adyacencia:");
+                    graph.printMatrix();
                     break;
                 case 5:
+                    System.out.println("\nMatriz de Floyd:");
+                    graph.distanceMatrix();
+                    break;
+                case 6:
                     exit = true;
                     break;
                 default: 
